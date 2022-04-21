c0dcf0320558e75c49714e73810395b5681bae84
Segun <adeyemioluwasegun98@gmail.com>
2022-04-21 09:23:06 +0100

    added new changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,12 +2,20 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.DB_NAME,
-  process.env.DB_PASSWORD,
+  // process.env.DB,
+  // process.env.DB_NAME,
+  // process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
     host: process.env.DB_HOST,
     dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     port: process.env.DB_PORT,
     define: {
       timestamps: false,
